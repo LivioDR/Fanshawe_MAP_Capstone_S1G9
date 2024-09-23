e2deab996faf17cdbf736b2fa957b6e912f0ac8f
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-23 19:43:04 -0400

    linked image from storage to UserBio screen minor styling changes

--- screens/UserBioScreen/UserBio.jsx
+++ screens/UserBioScreen/UserBio.jsx
@@ -8,6 +8,7 @@ import ProfileImage from "../../components/userBio/ProfileImage/ProfileImage";
 import NameRoleContainer from "../../components/userBio/NameRoleContainer/NameRoleContainer";
 import TextWithLabel from "../../components/common/TextWithLabel/TextWithLabel";
 import AvailablePTO from "../../components/userBio/AvailablePTO/AvailablePTO";
+import { getImageForUserId } from "../../services/database/profileImage";
 
 
 const UserBio = () => {
@@ -35,7 +36,7 @@ const UserBio = () => {
         // get team data
         setTeamData(teamBioData)
         // get image
-        // setImgUrl()
+        // getImageForUserId(userRetrievedData.uid).then(img => setImgUrl(img))
     },[])
 
 
@@ -45,14 +46,16 @@ const UserBio = () => {
             <ProfileImage url={imgUrl} />
             <NameRoleContainer name={`${userData.firstName} ${userData.lastName}`} role={userData.role} />
         </View>
-        <TextWithLabel label={'Corporate email'} textValue={userData.email} />
-        <TextWithLabel label={'Address'} textValue={userData.address} />
-        <TextWithLabel label={'Birth Date'} textValue={userData.birthday} />
+        <View style={bioStyles.body}>
+            <TextWithLabel label={'Corporate email'} textValue={userData.email} />
+            <TextWithLabel label={'Address'} textValue={userData.address} />
+            <TextWithLabel label={'Birth Date'} textValue={userData.birthday} />
 
-        <TextWithLabel label={'Team'} textValue={teamData.name} />
+            <TextWithLabel label={'Team'} textValue={teamData.name} />
 
-        <TextWithLabel label={'Supervisor name'} textValue={`${superData.firstName} ${superData.lastName}`} />
-        <TextWithLabel label={'Supervisor email'} textValue={superData.email} />
+            <TextWithLabel label={'Supervisor'} textValue={`${superData.firstName} ${superData.lastName}`} />
+            <TextWithLabel label={'Supervisor email'} textValue={superData.email} />
+        </View>
         
         <AvailablePTO numPto={12} numSick={4}/>
         </>
