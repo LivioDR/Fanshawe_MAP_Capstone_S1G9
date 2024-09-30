1fa5324def6746f8cb673b16219d052b1f988173
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-30 13:57:42 -0400

    Create basic empty home screen component

---
+++ screens/HomeScreen/index.js
@@ -0,0 +1,13 @@
+// RN components
+import { Text, View } from "react-native";
+
+// styles
+import styles from "./styles";
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+}
