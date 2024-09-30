3f3a324c18a806f2d6eecb1fd8adc8f3334335f5
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-30 14:55:34 -0400

    Add intro profile details

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,13 +1,39 @@
 // RN components
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 // styles
 import styles from "./styles";
 
 export default function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
+        <View style={styles.container.outer}>
+            {/* TODO: replace this with banner component */}
+            <View style={styles.banner}>
+                <Text style={styles.banner.text}>
+                    You have been clocked in since 8:59 am.
+                </Text>
+            </View>
+
+            <View style={styles.container.inner}>
+                <View style={styles.container.intro}>
+                    <View style={styles.container.image}>
+                        <Image
+                            style={styles.profileImage}
+                            source={require("../../assets/profile.png")}
+                        />
+                    </View>
+
+                    <Text style={styles.welcomeText}>
+                        {/* TODO: get name from user profile */}
+                        Welcome, Jonathan Handfeld Miller-Smith III.
+                    </Text>
+
+                    {/* TODO: make birthday banner display reactive */}
+                    <View style={styles.container.birthday}>
+                        <Text>🎉 Happy birthday! 🎉</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
--- screens/HomeScreen/styles.js
+++ screens/HomeScreen/styles.js
@@ -1,14 +1,80 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 import { safeAreaPadding } from "../../utilities/variables";
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center",
+        outer: {
+            flex: 1,
+            alignItems: "center",
 
-        paddingTop: safeAreaPadding,
+            paddingTop: safeAreaPadding,
 
-        backgroundColor: "#FFFFFF",
+            backgroundColor: "#FFF",
+        },
+
+        inner: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingHorizontal: 20,
+            gap: 85,
+        },
+
+        intro: {
+            gap: 15,
+            alignItems: "center",
+        },
+
+        image: {
+            width: 125,
+            alignItems: "center",
+
+            // make shadow darker on Android
+            shadowColor: Platform.OS === "ios" ? "#666" : "#000",
+            shadowOffset: {
+                width: 0,
+                height: 3,
+            },
+            shadowOpacity: 0.5,
+            shadowRadius: 2,
+            elevation: 4,
+
+            // need background color to show on Android
+            backgroundColor: "#0001",
+            // need border radius to round shadow on Android
+            borderRadius: 60,
+        },
+
+        birthday: {
+            backgroundColor: "#DDD",
+            padding: 10,
+            borderRadius: 30,
+        },
+    },
+
+    banner: {
+        width: "100%",
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+
+        backgroundColor: "#45CA44",
+
+        text: {
+            fontWeight: 500,
+            textAlign: "center",
+            color: "#FFF",
+        },
+    },
+
+    profileImage: {
+        height: 125,
+        width: 125,
+    },
+
+    welcomeText: {
+        fontWeight: "500",
+        fontSize: 24,
+        textAlign: "center",
     },
 });
