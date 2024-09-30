e68e0fd19cf878d0e2437d229d3a1efbfed8b860
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-30 15:29:00 -0400

    Add basic state for time clock buttons

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,3 +1,6 @@
+// React hooks
+import { useState } from "react";
+
 // RN components
 import { Image, Text, TouchableHighlight, View } from "react-native";
 
@@ -5,9 +8,12 @@ import { Image, Text, TouchableHighlight, View } from "react-native";
 import styles from "./styles";
 
 export default function HomeScreen() {
+    const [clockedIn, setClockedIn] = useState(false);
+    const [onLunch, setOnLunch] = useState(false);
+
     return (
         <View style={styles.container.outer}>
-            {/* TODO: replace this with banner component */}
+            {/* TODO: replace this with reactive banner component */}
             <View style={styles.banner}>
                 <Text style={styles.banner.text}>
                     You have been clocked in since 8:59 am.
@@ -32,36 +38,46 @@ export default function HomeScreen() {
                     <View style={styles.container.birthday}>
                         <Text>🎉 Happy birthday! 🎉</Text>
                     </View>
+
+                    <Text>Clocked in? {clockedIn ? "Yes" : "No"}</Text>
+                    <Text>On lunch? {onLunch ? "Yes" : "No"}</Text>
                 </View>
 
                 {/* TODO: factor buttons out into a component */}
                 <View style={styles.container.clockBtns}>
                     {/* TODO: replace with a custom shadowed button component */}
                     <TouchableHighlight
-                        style={styles.clockBtn}
+                        style={[styles.clockBtn, clockedIn ? undefined : styles.clockBtn.green]}
                         underlayColor="#DDD"
-                        onPress={() => {}}
+                        disabled={clockedIn}
+                        onPress={() => { setClockedIn(true); }}
                     >
                         <Text style={styles.clockBtn.text}>Clock In</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                        style={styles.clockBtn}
+                        style={[styles.clockBtn, !clockedIn ? undefined : styles.clockBtn.red]}
                         underlayColor="#DDD"
-                        onPress={() => {}}
+                        disabled={!clockedIn}
+                        onPress={() => {
+                            setClockedIn(false);
+                            setOnLunch(false);
+                        }}
                     >
                         <Text style={styles.clockBtn.text}>Clock Out</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                        style={styles.clockBtn}
+                        style={[styles.clockBtn, !clockedIn || onLunch ? undefined : styles.clockBtn.blue]}
                         underlayColor="#DDD"
-                        onPress={() => {}}
+                        disabled={!clockedIn || onLunch}
+                        onPress={() => { setOnLunch(true); }}
                     >
                         <Text style={styles.clockBtn.text}>Start Lunch</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                        style={styles.clockBtn}
+                        style={[styles.clockBtn, !(clockedIn && onLunch) ? undefined : styles.clockBtn.red]}
                         underlayColor="#DDD"
-                        onPress={() => {}}
+                        disabled={!(clockedIn && onLunch)}
+                        onPress={() => { setOnLunch(false); }}
                     >
                         <Text style={styles.clockBtn.text}>End Lunch</Text>
                     </TouchableHighlight>
