45c9c3c1c31d77a95737c6c98be12a3cfb98d0ff
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-30 15:14:11 -0400

    Add drop shadow global styles

--- utilities/variables.js
+++ utilities/variables.js
@@ -1,3 +1,15 @@
 import { Platform } from "react-native"
 
 export const safeAreaPadding = Platform.OS === "ios" ? 60 : 50;
+
+export const dropShadowStyle = {
+    // remove shadowColor on Android because it conflicts with elevation
+    shadowColor: Platform.OS === "ios" ? "#666" : undefined,
+    shadowOffset: {
+        width: 0,
+        height: 3,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 3,
+};
