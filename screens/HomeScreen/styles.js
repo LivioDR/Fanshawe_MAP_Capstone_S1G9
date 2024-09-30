0aee6fb3fb647978c945229265be22311b317bbb
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-30 14:09:17 -0400

    Pad safe area

--- screens/HomeScreen/styles.js
+++ screens/HomeScreen/styles.js
@@ -1,10 +1,13 @@
 import { StyleSheet } from "react-native";
 
+import { safeAreaPadding } from "../../utilities/variables";
+
 export default StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
+
+        paddingTop: safeAreaPadding,
 
         backgroundColor: "#FFFFFF",
     },
