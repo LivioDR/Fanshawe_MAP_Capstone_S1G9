7d7eb41dc9ae37ff15cc26caf9a6465b36cd4944
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-10-01 13:03:44 -0400

    modified setImageForUserId function based on documentation

--- services/database/profileImage.js
+++ services/database/profileImage.js
@@ -1,5 +1,5 @@
 import { storage } from "../../config/firebase";
-import { ref, getDownloadURL } from "firebase/storage";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 
 const getImageForUserId = async(id) => {
     let urlPath = undefined
@@ -14,19 +14,42 @@ const getImageForUserId = async(id) => {
     return urlPath
 }
 
-const setImageForUserId = async(id, file, setLoading) => {
+// Based on the Expo Firebase tutorial:
+// https://docs.expo.dev/versions/latest/sdk/imagepicker/
+const setImageForUserId = async(id, uri, setLoading) => {
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+
+    setLoading(true)
+
     try{
-        setLoading(true)
-        const pathRef = ref(storage, `users/${id}/profile.jpg`)
-        uploadBytes(pathRef``, file).then((snapshot) => {
-            console.log('Uploaded a blob or file!');
-            console.log(snapshot)
+        const blob = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError("Network request failed"));
+          };
+          xhr.responseType = "blob";
+          xhr.open("GET", uri, true);
+          xhr.send(null);
         });
+      
+        const fileRef = ref(storage, `users/${id}/profile.jpg`)
+        const result = await uploadBytes(fileRef, blob);
+      
+        // We're done with the blob, close and release it
+        blob.close();
     }
     catch(e){
         console.error(e)
     }
     setLoading(false)
-}
+  
+    return await getImageForUserId(id);
+  }
+  // End of tutorial function
 
 export { getImageForUserId, setImageForUserId }
