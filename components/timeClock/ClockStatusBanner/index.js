bfe10c563d612dd3e0a95725d14d43cfe084eafb
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-03 14:24:49 -0400

    Use time from database in banner

--- components/timeClock/ClockStatusBanner/index.js
+++ components/timeClock/ClockStatusBanner/index.js
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 
+import { Timestamp } from "firebase/firestore";
+
 import styles from "./styles";
 
 export default function ClockStatusBanner({ clockStatus }) {
-    // TODO: replace with time values from status
-    const clockInTime = "8:59 am";
-    const lunchTime = "12:01 pm";
+    const clockInTime = clockStatus.clockInTime ? timestampToTimeString(clockStatus.clockInTime) : "";
+    const lunchTime = clockStatus.onLunchTime ? timestampToTimeString(clockStatus.onLunchTime) : "";
 
     let bannerStyle = styles.banner.clockedOut;
     let bannerText = "You are not clocked in.";
@@ -24,4 +25,27 @@ export default function ClockStatusBanner({ clockStatus }) {
             </Text>
         </View>
     );
+}
+
+/**
+ * Given a Firebase Timestamp, convert it to a displayable time string.
+ * @param {Timestamp} time Firebase Timestamp object
+ * @returns formatted string representing the time
+ */
+function timestampToTimeString(stamp) {
+    return stamp
+            .toDate()   // convert to JS Date object
+            // convert to a locale string
+            .toLocaleTimeString([],
+                {
+                    // use 12-hour time
+                    hour12: true,
+                    // show number as numeric to omit leading 0's
+                    hour: "numeric",
+                    // always show 2-digit minute
+                    minute: "2-digit",
+                }
+            )
+            // lowercase the AM/PM
+            .toLowerCase();
 }
