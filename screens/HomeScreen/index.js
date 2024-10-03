edeb396821d40dd2e2dd19456836a42254cf9848
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-03 19:11:21 -0400

    Add clocking in

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -10,7 +10,7 @@ import ClockButtons from "../../components/timeClock/ClockButtons";
 
 // database
 import { Timestamp } from "firebase/firestore";
-import { getOpenTimeLog, updateTimeLog } from "../../services/database/timeClock";
+import { createTimeLog, getOpenTimeLog, updateTimeLog } from "../../services/database/timeClock";
 
 // styles
 import styles from "./styles";
@@ -22,6 +22,7 @@ export default function HomeScreen() {
     const [timeLog, setTimeLog] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    // async effect to load the user's current time log, if one exists
     useEffect(() => {
         (async () => {
             // TODO: update hardcoded user id
@@ -33,7 +34,7 @@ export default function HomeScreen() {
                 // only set up state if we have a clock in time and have not clocked out
                 if (curTimeLog.clockInTime && !curTimeLog.clockOutTime) {
                     setClockedIn(true);
-    
+
                     // set up lunch state
                     if (curTimeLog.offLunchTime) {
                         setTakenLunch(true);
@@ -87,10 +88,15 @@ export default function HomeScreen() {
     // action functions for the clock buttons
     const buttonActions = {
         clockIn: async () => {
-            // assume we will succeed and update state
-            setClockedIn(true);
-
-            // TODO: create a new time log and write it to the database
+            // TODO: update hardcoded user id
+            const newTimeLog = await createTimeLog("user1234", Timestamp.now());
+            if (newTimeLog) {
+                setTimeLog(newTimeLog);
+                setClockedIn(true);
+            } else {
+                // TODO: handle errors more elegantly
+                console.error("Something went wrong, try again later.");
+            }
         },
 
         clockOut: async () => {
@@ -158,6 +164,7 @@ export default function HomeScreen() {
         },
     };
 
+    // get the content to display based on loading status
     const content = loading ?
         <>
             <ActivityIndicator size="large" />
