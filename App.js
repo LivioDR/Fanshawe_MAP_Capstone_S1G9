b3f30ffec3666adec1d162f7c8478318020a700d
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-06 17:05:47 -0400

    Conditionally render login or main screens

--- App.js
+++ App.js
@@ -8,6 +8,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// hooks
+import { useState } from 'react';
+
 // icons
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
@@ -18,6 +21,7 @@ import UserBio from "./screens/UserBioScreen/UserBio";
 
 // theme variables
 import { accent, highlight } from './utilities/variables';
+import LoginScreen from './screens/LoginScreen';
 
 // create bottom tab navigator elements
 const Tab = createBottomTabNavigator();
@@ -30,9 +34,6 @@ const NavTheme = {
     }
 };
 
-// TODO: replace this with an auth variable of some sort
-const loggedIn = true;
-
 // TODO: replace these placeholder components with screen components from screens
 function CompanyScreenPlaceholder() {
     return (
@@ -43,64 +44,75 @@ function CompanyScreenPlaceholder() {
 }
 
 export default function App() {
-    return (
-        <NavigationContainer theme={NavTheme}>
-            <StatusBar style="auto" />
+    const [loginCredential, setLoginCredential] = useState(null);
 
-            <Tab.Navigator
-                initialRouteName="Home"
-                screenOptions={{
-                    headerShown: false,
-                    tabBarActiveTintColor: accent,
-                    tabBarStyle: {
-                        backgroundColor: highlight,
-                    },
-                }}
-            >
-                <Tab.Screen
-                    name="Company"
-                    component={CompanyScreenPlaceholder}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialIcons
-                                name="apartment"
-                                size={size}
-                                color={color}
-                            />
-                        ),
-                    }}
-                />
+    const onLogin = (credential) => {
+        console.log(credential);
+        setLoginCredential(credential);
+    };
 
-                <Tab.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        tabBarIcon: ({ focused, color, size }) => (
-                            <MaterialCommunityIcons
-                                name={focused ? "home-variant" : "home-variant-outline"}
-                                size={size}
-                                color={color}
-                            />
-                        ),
+    if (loginCredential) {
+        return (
+            <NavigationContainer theme={NavTheme}>
+                <StatusBar style="auto" />
+    
+                <Tab.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        headerShown: false,
+                        tabBarActiveTintColor: accent,
+                        tabBarStyle: {
+                            backgroundColor: highlight,
+                        },
                     }}
-                />
-
-                <Tab.Screen
-                    name="Profile"
-                    component={UserBio}
-                    options={{
-                        tabBarIcon: ({ focused, color, size }) => (
-                            <MaterialIcons
-                                name={focused ? "person" : "person-outline"}
-                                size={size}
-                                color={color}
-                            />
-                        ),
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+                >
+                    <Tab.Screen
+                        name="Company"
+                        component={CompanyScreenPlaceholder}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <MaterialIcons
+                                    name="apartment"
+                                    size={size}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+    
+                    <Tab.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{
+                            tabBarIcon: ({ focused, color, size }) => (
+                                <MaterialCommunityIcons
+                                    name={focused ? "home-variant" : "home-variant-outline"}
+                                    size={size}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+    
+                    <Tab.Screen
+                        name="Profile"
+                        component={UserBio}
+                        options={{
+                            tabBarIcon: ({ focused, color, size }) => (
+                                <MaterialIcons
+                                    name={focused ? "person" : "person-outline"}
+                                    size={size}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        );
+    } else {
+        return <LoginScreen loginSuccess={onLogin} />;
+    }
 }
 
 const styles = StyleSheet.create({
