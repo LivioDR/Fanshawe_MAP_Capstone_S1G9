52278db70420d203a096975da3ff02e6ed8291d3
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-10-07 15:35:27 -0400

    padding adjustment

--- screens/UserBioScreen/UserBioStyles.js
+++ screens/UserBioScreen/UserBioStyles.js
@@ -6,7 +6,6 @@ export default UserBioStyles = StyleSheet.create({
     },
     body: {
         width: '100%',
-        // paddingVertical: 12,
     },
     buttonsWrapper: {
         width: '100%',
