464d14f8574749bee50ae155437c7e6ac97aa644
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-15 14:31:08 -0400

    Add a lighter, more subtle drop shadow style

--- utilities/variables.js
+++ utilities/variables.js
@@ -14,6 +14,18 @@ export const dropShadowStyle = {
     elevation: 3,
 };
 
+export const lightDropShadowStyle = {
+    // remove shadowColor on Android because it conflicts with elevation
+    shadowColor: Platform.OS === "ios" ? "#999" : undefined,
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+};
+
 // colors
 export const highlight = "#EFEFEF";
 export const positive = "#45CA44";
