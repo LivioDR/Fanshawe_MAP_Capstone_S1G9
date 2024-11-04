5aca6c076958452d6d7894e21307f8f27bd8162e
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-04 14:49:40 -0500

    Add credentials slice and reducer

--- services/state/store.js
+++ services/state/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import userCredentialsReducer from "./userCredentialsSlice";
+
 export default store = configureStore({
     reducer: {
-        
+        credentials: userCredentialsReducer,
     },
 });
