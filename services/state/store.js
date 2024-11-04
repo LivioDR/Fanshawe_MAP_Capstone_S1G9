6fff439ef007acc101fe869a772a173b529c538e
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-04 15:23:44 -0500

    Revert "Add credentials slice and reducer"
    This reverts commit 5aca6c076958452d6d7894e21307f8f27bd8162e.

--- services/state/store.js
+++ services/state/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import userCredentialsReducer from "./userCredentialsSlice";
-
 export default store = configureStore({
     reducer: {
-        credentials: userCredentialsReducer,
+        
     },
 });
