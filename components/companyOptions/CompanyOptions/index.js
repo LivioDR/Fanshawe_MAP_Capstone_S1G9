da6b6cd29fb0d40aeefb33d8c00859c00e16fa70
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-12 18:22:36 -0500

    added conditional rendering for admin menu

--- components/companyOptions/CompanyOptions/index.js
+++ components/companyOptions/CompanyOptions/index.js
@@ -6,7 +6,19 @@ import CompanyOptionSelector from "./CompanyOptionSelector";
 
 import styles from "./styles";
 
+import { useCredentials } from "../../../services/state/userCredentials";
+import { useBioInfo } from "../../../services/state/userBioInfo";
+
+
 const options = [
+    {
+        caption: "My Team",
+        destination: "MyTeamScreen",
+        icon: <MaterialIcons name="people" size={30} color="black" />,
+    },
+];
+
+const adminOptions = [
     {
         caption: "My Team",
         destination: "MyTeamScreen",
@@ -15,15 +27,21 @@ const options = [
     {
         caption: "Manage Team",
         destination: "ManageTeamScreen",
-        icon: <MaterialIcons name="people" size={30} color="black" />,
+        icon: <MaterialIcons name="cases" size={30} color="black" />,
     },
 ];
 
 export default function CompanyOptions() {
+    
+    const userCredentials = useCredentials()
+    const userBios = useBioInfo()
+
+    let isUserAdmin = userBios.bios[userCredentials.user.uid].isSupervisor
+
     return (
         <View style={styles.container}>
             <FlatList
-                data={options}
+                data={isUserAdmin? adminOptions : options}
                 renderItem={(item) => <CompanyOptionSelector {...item.item} />}
                 contentContainerStyle={styles.listContainer}
             />
