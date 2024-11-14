7f09b1a8092d5b344b6433c9a19ffb99ea1dc459
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-14 09:59:52 -0500

    Allow use before translations have loaded

--- components/common/LoadingIndicator/index.js
+++ components/common/LoadingIndicator/index.js
@@ -4,13 +4,16 @@ import { View, Text, ActivityIndicator } from "react-native";
 
 import styles from "./styles";
 
-export default function LoadingIndicator() {
-    const { t } = useTranslation();
+export default function LoadingIndicator({ textOverride }) {
+    let t;
+    if (!textOverride) {
+        t = useTranslation().t;
+    }
 
     return (
         <View style={styles.container}>
             <ActivityIndicator size="large" />
-            <Text style={styles.text}>{t("common.loading")}</Text>
+            <Text style={styles.text}>{!!textOverride ? textOverride : t("common.loading")}</Text>
         </View>
     );
 }
