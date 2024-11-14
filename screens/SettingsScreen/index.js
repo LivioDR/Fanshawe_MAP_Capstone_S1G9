1d71305d780c838961d91e42ee1ec0eda0280841
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-14 10:40:06 -0500

    Style settings screen

--- screens/SettingsScreen/index.js
+++ screens/SettingsScreen/index.js
@@ -1,9 +1,15 @@
-import { View, Text, Button } from "react-native";
-import { Dropdown } from "react-native-element-dropdown";
-import { currentLngKey, supportedLanguages } from "../../services/i18n/i18n";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { currentLngKey, supportedLanguages } from "../../services/i18n/i18n";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { useEffect, useState } from "react";
+
+import { View, Text, Button } from "react-native";
+
+import { Dropdown } from "react-native-element-dropdown";
+
+import styles from "./styles";
 
 export default function SettingsScreen() {
     const [languages] = useState(getLanguagesList());
@@ -24,22 +30,33 @@ export default function SettingsScreen() {
     };
 
     return (
-        <View>
-            <Text>Language</Text>
-            <Dropdown
-                data={languages}
-                labelField="label"
-                valueField="value"
-                value={i18n.language}
-                onChange={onLanguageChange}
-            />
+        <View style={styles.container}>
+            <View style={styles.settingContainer}>
+                <Text style={styles.heading}>Language</Text>
+                <Dropdown
+                    style={styles.dropdown}
+
+                    data={languages}
+                    labelField="label"
+                    valueField="value"
+
+                    search
+                    searchField="label"
+                    searchPlaceholder="Search..."
+
+                    value={i18n.language}
+                    onChange={onLanguageChange}
+                />
+            </View>
 
             {/* TODO: remove debug */}
-            <Text>Stored language: {!!storedLanguage ? storedLanguage : "none"}</Text>
-            <Button title="Clear saved language" onPress={async () => {
-                await AsyncStorage.removeItem(currentLngKey);
-                setStoredLanguage(null);
-            }} />
+            <View style={styles.settingContainer}>
+                <Text>Stored language: {!!storedLanguage ? storedLanguage : "none"}</Text>
+                <Button title="Clear saved language" onPress={async () => {
+                    await AsyncStorage.removeItem(currentLngKey);
+                    setStoredLanguage(null);
+                }} />
+            </View>
         </View>
     );
 }
