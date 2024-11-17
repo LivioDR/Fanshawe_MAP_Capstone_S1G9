96690392918c87e54cdbe368df229a56d912e698
Aggrey Chido Nhiwatiwa <78484064+AggreyNhiwatiwa@users.noreply.github.com>
2024-11-17 13:22:47 -0500

    Update index.js
    Reverted to previous commit commit due to redundant PTOAdmin state code

--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -11,13 +11,8 @@ import { getUserBioInfoById } from "../../services/database/userBioInfo";
 import InputMsgBox from "../../components/InputMsgBox";
 import { auth } from "../../config/firebase";
 import UiButton from "../../components/common/UiButton/UiButton";
-import { usePTOAdmin } from "../../services/state/ptoAdmin";
 
 export default function LoginScreen({ loginSuccess }) {
-
-  /* Getting state from the usePTOAdmin */
-  const { updatePTOAdmin } = usePTOAdmin();
-
   /* States */
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
@@ -29,9 +24,7 @@ export default function LoginScreen({ loginSuccess }) {
     useState(true);
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [pwdIsValid, setPwdIsValid] = useState(false);
-
   /* Hooks */
-
   /*
   Ensures that there are no active users signed in when the login page is entered
   */
@@ -45,34 +38,27 @@ export default function LoginScreen({ loginSuccess }) {
         }
     })();
   }, []);
-
   /*
   Tracks whenever the username or pwd changes and conducts the sanity check
   */
   useEffect(() => {
     updateLoginButtonState();
   }, [emailIsValid, pwdIsValid]);
-
   /* Handlers */
-
   const handleForgotPasswordPress = () => {
     handleModalToggle();
   };
-
   const handleModalToggle = () => {
     setShowModal(!showModal);
   };
-
   /*
   Sanity check for email
   Regex pattern obtained via https://regexr.com/
   */
   const handleEmailChange = (value) => {
     setEmail(value);
-
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     const emailRegexTest = emailRegex.test(value);
-
     if (emailRegexTest == false) {
       setEmailIsValid(false);
       setEmailErrTxt("Please enter a valid email");
@@ -83,11 +69,9 @@ export default function LoginScreen({ loginSuccess }) {
       setPasswordResetBtnDisabled(false);
     }
   };
-
   /* Sanity check for pwd */
   const handlePwdChange = (value) => {
     setPwd(value);
-
     if (value.length === 0) {
       setPwdIsValid(false);
       setPwdErrTxt("Please enter a password");
@@ -96,7 +80,6 @@ export default function LoginScreen({ loginSuccess }) {
       setPwdErrTxt("");
     }
   };
-
   /*
   Attempts to sign user in to db
   */
@@ -110,139 +93,6 @@ export default function LoginScreen({ loginSuccess }) {
         if(userInfo.isEnabled){
           showSuccessToast("Login successful");
           loginSuccess(userCredential);
-
-          // Get info of logged in user to determine if they're an admin
-          // for the Admin's PTO functionality
-          // if(userInfo.isSupervisor){
-          //   updatePTOAdmin({ inAdminMode: true });
-          // }
-
         }
         else{
           showErrorToast("User disabled. Please contact your administrator")
-          await signOut(auth)
-        }
-      })
-      .catch(() => {
-        showErrorToast("Incorrect username or password");
-        handlePwdChange("");
-      });
-  };
-
-  /*
-  Sends a password reset email if the email is registered in the DB
-  Due to security, theres no way in Firebase to sanity check whether an email is in the DB 
-  before the request is made
-  */
-  const handleSendPasswordResetLink = () => {
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        showSuccessToast("Password reset via email requested");
-      })
-      .catch(() => {
-        showErrorToast(
-          "There was an error, sending the link, please try again"
-        );
-      });
-  };
-
-  /*
-    Helper function for changing the login button state depending
-    on whether the user input is valid
-  */
-  const updateLoginButtonState = () => {
-    if (emailIsValid && pwdIsValid) {
-      setLoginBtnDisabled(false);
-    } else {
-      setLoginBtnDisabled(true);
-    }
-  };
-
-  /* Toast logic */
-  const showSuccessToast = (msg) => {
-    Toast.show({
-      type: "success",
-      text1: "Success ✅",
-      text2: msg,
-      position: "bottom",
-    });
-  };
-
-  const showErrorToast = (errMsg) => {
-    Toast.show({
-      type: "error",
-      text1: "Error 🛑",
-      text2: errMsg,
-      visibilityTime: 2200,
-      position: "bottom",
-    });
-  };
-
-  return (
-    <>
-      <View style={styles.container}>
-        <Toast />
-        <TextInput
-          style={styles.textInputContainer}
-          placeholder="Email Address"
-          onChangeText={handleEmailChange}
-          keyboardType={"email"}
-          autoCapitalize="none"
-        />
-
-        <InputMsgBox text={emailErrTxt}></InputMsgBox>
-
-        <TextInput
-          style={styles.textInputContainer}
-          placeholder="Password"
-          onChangeText={handlePwdChange}
-          secureTextEntry={true}
-          value={pwd}
-        />
-
-        <InputMsgBox text={pwdErrTxt}></InputMsgBox>
-
-        <UiButton
-          label="Login"
-          funcToCall={handleLoginPress}
-          disabled={loginBtnDisabled}
-          type="CTA"
-        ></UiButton>
-
-        <Button
-          title="Forgotten Password?"
-          onPress={handleForgotPasswordPress}
-        ></Button>
-
-        <View style={styles.footer}>
-          <Text>Powered by IndusTree 🌳 © Copyright 2024</Text>
-        </View>
-
-        <Modal animationType="slide" visible={showModal}>
-          <View style={styles.modalView}>
-            <TextInput
-              style={styles.textInputContainer}
-              placeholder="Email Address"
-              onChangeText={handleEmailChange}
-              value={email} 
-              keyboardType={"email"}
-              autoCapitalize="none"
-            />
-
-            <InputMsgBox text={emailErrTxt}></InputMsgBox>
-
-            <UiButton
-              label="Send Password Reset Link"
-              funcToCall={handleSendPasswordResetLink}
-              disabled={passwordResetBtnDisabled}
-              type="CTA"
-            ></UiButton>
-
-            <Button title="Close" onPress={handleModalToggle}></Button>
-          </View>
-          <Toast />
-        </Modal>
-      </View>
-    </>
-  );
-}
